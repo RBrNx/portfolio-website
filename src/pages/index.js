b2fa48659d2c802214fd1dffa095aca590fbf271
Conor Watson <conor.watson94@gmail.com>
2021-12-26 18:04:24 +0000

    feat: add Section to Portfolio page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,43 @@ import DefaultLayout from '../layouts/Default';
 import HomeHeaderImage from '../images/hero/home-header.jpeg';
 import PortfolioTitle from '../features/portfolio/Title';
 import PortfolioSubtitle from '../features/portfolio/Subtitle';
+import Section from '../library/components/Section';
 
 const IndexPage = () => (
-  <DefaultLayout heroImage={HomeHeaderImage} heroTitle={PortfolioTitle} heroSubtitle={PortfolioSubtitle} />
+  <DefaultLayout heroImage={HomeHeaderImage} heroTitle={PortfolioTitle} heroSubtitle={PortfolioSubtitle}>
+    <Section title='Portfolio' subtitle='Minigolf Games, Christmas Puzzle Events, A Wedding App and much more!'>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+      <div>Here is some text</div>
+    </Section>
+  </DefaultLayout>
 );
 
 export default IndexPage;
