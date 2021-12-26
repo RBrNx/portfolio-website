519aa81574076ea40af4b0dcb455b9bb7a41aa0f
Conor Watson <conor.watson94@gmail.com>
2021-12-26 14:16:06 +0000

    feat: create root layout with global styles and Styled-Components theme

---
+++ src/layouts/Root.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+// Define what props.theme will look like
+const theme = {
+  background: '#1d1d1d',
+  accent: '#c32626',
+  heading: '#cacaca',
+  body: '#b5b5b5',
+};
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <ThemeProvider theme={theme}>
+    {children}
+    <GlobalStyles />
+  </ThemeProvider>
+);
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    background-color: ${props => props.theme.background};
+  }
+`;
+
+export default RootLayout;
