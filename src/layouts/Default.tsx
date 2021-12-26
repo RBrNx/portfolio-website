e0faf0d4deb06bd027d517e938441ba5be8ed845
Conor Watson <conor.watson94@gmail.com>
2021-12-26 16:23:30 +0000

    feat: use HeroHeader component in DefaultLayout

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import Navbar from '../features/navigation/Navbar';
+import HeroHeader from '../library/components/HeroHeader';
 
-const DefaultLayout = () => (
+interface DefaultLayoutProps {
+  children: React.ReactNode;
+  heroImage: string;
+  heroTitle: () => JSX.Element;
+  heroSubtitle: () => JSX.Element;
+}
+
+const DefaultLayout = ({ children, heroImage, heroTitle, heroSubtitle }: DefaultLayoutProps) => (
   <div>
     <Navbar />
+    <main>
+      <HeroHeader image={heroImage} title={heroTitle} subtitle={heroSubtitle} />
+      {children}
+    </main>
   </div>
 );
 
