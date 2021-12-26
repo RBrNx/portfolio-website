506fbb37f28882c149776ef56b8c09cf43ff84b7
Conor Watson <conor.watson94@gmail.com>
2021-12-26 14:16:48 +0000

    feat: create Default layout and use it on the index page

---
+++ src/layouts/Default.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from '../features/navigation/Navbar';
+
+const DefaultLayout = () => (
+  <div>
+    <Navbar />
+  </div>
+);
+
+export default DefaultLayout;
