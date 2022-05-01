4559e1fb8e147435f3e85afa390c8b97cf646822
Conor Watson <conor.watson94@gmail.com>
2022-05-01 15:43:26 +0100

    fix: avoid window crash during SSR build

--- src/features/blog/BlogArticle.tsx
+++ src/features/blog/BlogArticle.tsx
@@ -57,7 +57,7 @@ const BlogArticle = ({ title, description, headerImageUrl, content, publishedAt 
             </AuthorInfo>
           </BlogMetadata>
           <SocialShare
-            url={window.location.href}
+            url={window && window.location.href}
             title={title}
             description={description}
             headerImageUrl={headerImageUrl}
