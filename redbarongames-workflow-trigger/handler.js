2180b6cbe0944ed02060f7fbd56e9ca89e9615c0
Conor Watson <conor.watson94@gmail.com>
2022-07-03 11:19:12 +0100

    feat: add signed header support to workflow trigger

--- redbarongames-workflow-trigger/handler.js
+++ redbarongames-workflow-trigger/handler.js
@@ -1,8 +1,22 @@
 'use strict';
 
 const axios = require('axios')
+const { verifyWebhookSignature } = require('@graphcms/utils');
 
 module.exports.trigger = async (event) => {
+  const { SECRET_KEY } = process.env;
+  const { body, headers } = event;
+  const signature = headers['gcms-signature'];
+
+  const isValidRequest = verifyWebhookSignature({ body, signature, secret: SECRET_KEY });
+
+  if(!isValidRequest) {
+    return {
+      statusCode: 401,
+      body: 'Unauthorized - Invalid Request'
+    }
+  }
+
   const { status } = await axios.post(
     'https://api.github.com/repos/RBrNx/portfolio-website/actions/workflows/deploy-gatsby-site.yml/dispatches',
     { ref: 'master' },
